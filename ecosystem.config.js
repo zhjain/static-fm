231b574e413e5122fce7a79dcd2b95923a3035b2
Milky <1099738693@qq.com>
2025-09-24 16:58:57 +0800

    chore(config): 增加 icecast 和 liquidsoap 应用配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,38 @@
 module.exports = {
-  apps : [{
-    name   : "static-fm",
-    script : "./server.js",
-    instances : 1,
-    exec_mode : "fork",
-    watch: false,
-    max_memory_restart: "1G",
-    env: {
-      NODE_ENV: "development",
-      PORT: 3000
+  apps : [
+    {
+      name   : "static-fm",
+      script : "./server.js",
+      instances : 1,
+      exec_mode : "fork",
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000
+      },
+      env_production : {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
     },
-    env_production : {
-      NODE_ENV: "production",
-      PORT: 3000
+    {
+      name: "icecast",
+      script: "icecast",
+      args: "-c ./config/icecast.xml -b",
+      interpreter: "none",
+      env: {
+        NODE_ENV: "development"
+      }
+    },
+    {
+      name: "liquidsoap",
+      script: "liquidsoap",
+      args: "-d ./config/radio.liq",
+      interpreter: "none",
+      env: {
+        NODE_ENV: "development"
+      }
     }
-  }]
+  ]
 }
